e7e7ab95fd8ffdd637a75bedbeb25378e38ad477
Alvarodevs <alvaro.develops@gmail.com>
2020-12-23 15:46:52 +0000

    Layout updated

--- src/main.js
+++ src/main.js
@@ -1,10 +1,48 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+// /* eslint-disable */
+// import "bootstrap";
+// import "./style.css";
+// import { doc } from "prettier";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// window.onload = function() {
+//   var cellstoFire = document.querySelectorAll(".cells");
 
-window.onload = function() {
-  console.log("Hello Rigo from the console!");
-};
+//   var touched = (celltoFire.style.backgroundColor = "red");
+
+//   var water = (celltoFire.style.backgroundColor = "blue");
+
+//   var sunken = (celltoFire.style.backgroundColor = "darkblue");
+
+//   //LOOP OF NESTED ARRAYS & FIRETORPEDO FUNCTION
+
+//   function fireTorpedo() {
+//     for (var i = 0; i < gameBoard.length; i++) {
+//       for (var j = 0; j < gameBoard[i].length; j++) {
+//         if (gameBoard[i][j] == 0) {
+//           return water;
+//         } else {
+//           return touched;
+//         }
+//       }
+//     }
+
+//     let gameBoard = [
+//       [1, 1, 1, 1, 1, 0, 0, 0, 0, 1],
+//       [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+//       [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+//       [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+//       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//       [1, 0, 0, 1, 1, 0, 0, 0, 0, 0],
+//       [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//       [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//       [1, 1, 1, 1, 0, 0, 0, 0, 0, 0]
+//     ];
+
+//     cellstoFire.addEventListener("click", fireTorpedo);
+
+//     // 0 = empty
+//     // 1 = part of a ship
+//     // 2 = a sunken part of a ship
+//     // 3 = a missed shot
+//   }
+// };
